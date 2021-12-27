d07f2635a72d3224f62c58dca73d8a671b650dd1
SodiumN1trate <renarsgausins21@gmail.com>
2021-12-27 18:55:56 +0200

    .

--- index.php
+++ index.php
@@ -14,10 +14,6 @@ $router->add('/email_list', 'Page::client');
 $router->add('/add_email', 'Email::addEmail');
 $router->add('/get_email_list', 'Email::getEmails');
 $router->add('/get_host_list', 'Email::getHosts');
-$router->add('/sorted_by_name', 'Email::getEmailsSortedByName', 'POST');
-$router->add('/sorted_by_host', 'Email::getEmailsSortedByHost', 'POST');
-$router->add('/sorted_by_date', 'Email::getEmailsSortedByDate', 'POST');
-$router->add('/search_email', 'Email::searchEmail', 'POST');
 $router->add('/delete_email', 'Email::deleteEmail', 'POST');
 
 $router->submit();
